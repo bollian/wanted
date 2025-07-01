bdc14ef371be91c968103a39698225a111bbd404
Ian Boll <mainbollian@gmail.com>
2025-06-30 21:44:09 -0400

    feat: add useful base packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,8 +9,30 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+# RPMFusion for nonfree packages
+dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+packages=() # array of additional packages to install
+
+# hardware enablement
+packages+=(mesa-va-drivers-freeworld)
+
+# basic dev tools
+packages+=(gcc clang flatpak-builder git-lfs java)
+
+# VM support
+packages+=(libvirt libvirt-daemon-config-network libvirt-daemon-kvm virt-install qemu-kvm)
+
+# sysadmin tools
+packages+=(fzf htop tailscale wireguard-tools wl-clipboard)
+
+# install gaming tools
+packages+=(steam mangohud)
+
+dnf install -y ${packages[@]}
+
+dnf remove -y toolbox
+dnf autoremove -y
 
 # Use a COPR Example:
 #
